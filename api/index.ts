a026236d52b4795430adea0d459e6605cc1a9e51
Chayanon Thongpila <nutchy.th@gmail.com>
2021-07-28 13:45:52 +0700

    fix: add todo

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,9 @@ import { SubmitEventPayload } from './../interfaces/index'
 
 class ApiClient {
   submitEvent(payload: SubmitEventPayload) {
+    /**
+     * @todo - change api endpoint
+     */
     return fetch('https://activity.ywc18.ywc.in.th/api/', {
       method: 'POST',
       body: JSON.stringify(payload)
