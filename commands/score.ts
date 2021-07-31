becbc5eb08f3df8f4ffaa647d7e5fa103367f42e
Wiput Pootong <wiput.pootong@gmail.com>
2021-07-31 16:49:50 +0700

    feat(score): submit score

--- commands/score.ts
+++ commands/score.ts
@@ -1,4 +1,4 @@
-import Discord from 'discord.js'
+import fetch from 'node-fetch'
 import { CHANNELS, ROLE } from '../config'
 
 import { BaseCommander } from './base'
@@ -23,6 +23,11 @@ export class ScoreCommander extends BaseCommander {
     const user = this.message.mentions.users.first()
     const role = this.message.mentions.roles.first()
 
+    if (!user && !role) {
+      await this.message.reply(`เพิ่มตะแนนไม่สำเร็จ`)
+      return
+    }
+
     let userIds: string[] = []
 
     if (role) {
@@ -40,8 +45,35 @@ export class ScoreCommander extends BaseCommander {
       userIds = [user.id]
     }
 
-    const mention = user ? `<@${user?.id}>` : `<@&${role?.id}>`
-    // Reply with mentioned
-    await this.message.channel.send(`${mention} ได้แต้มบุญเพิ่ม ${score} แต้ม`)
+    const body = {
+      userIds,
+      score: Number.parseInt(score),
+      submitBy: this.message.author.id,
+    }
+
+    const headers = {
+      Authorization: process.env.INTERNAL_TOKEN,
+      'Content-Type': 'application/json',
+    } as { [key: string]: string }
+
+    const response = await fetch('https://activity.ywc18.ywc.in.th/api/score', {
+      headers,
+      method: 'POST',
+      body: JSON.stringify(body),
+    })
+
+    const data = await response.json()
+
+    if (data.success) {
+      const mention = user ? `<@${user?.id}>` : `<@&${role?.id}>`
+      // Reply with mentioned
+      await this.message.channel.send(
+        `${mention} ได้แต้มบุญเพิ่ม ${score} แต้ม`
+      )
+
+      return
+    } else {
+      await this.message.reply(`เพิ่มตะแนนไม่สำเร็จ`)
+    }
   }
 }
